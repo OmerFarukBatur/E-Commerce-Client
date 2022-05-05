9a268d7f266d5b0709a3d7d17a9e5cb55369d97b
OmerFarukBatur <44163877+OmerFarukBatur@users.noreply.github.com>
2022-05-05 22:16:11 +0300

    product list created

--- src/app/services/common/http-client.service.ts
+++ src/app/services/common/http-client.service.ts
@@ -21,7 +21,7 @@ export class HttpClientService {
     }
     else
     {
-      url = `${this.url(requestParameter)}${id ? `/${id}` : ""}`;
+      url = `${this.url(requestParameter)}${id ? `/${id}` : ""}${requestParameter.queryString ? `?${requestParameter.queryString}` : ""}`;
     }
    return this.httpClient.get<T>(url,{headers: requestParameter.headers});
   }
@@ -35,7 +35,7 @@ export class HttpClientService {
     }
     else
     {
-      url = `${this.url(requestParameter)}`;
+      url = `${this.url(requestParameter)}${requestParameter.queryString ? `?${requestParameter.queryString}` : ""}`;
     }
     return this.httpClient.post<T>(url,body, {headers: requestParameter.headers});
   }
@@ -49,7 +49,7 @@ export class HttpClientService {
     }
     else
     {
-      url = `${this.url(requestParameter)}`;
+      url = `${this.url(requestParameter)}${requestParameter.queryString ? `?${requestParameter.queryString}` : ""}`;
     }
     return this.httpClient.put<T>(url,body,{headers: requestParameter.headers});
   }
@@ -63,7 +63,7 @@ export class HttpClientService {
     }
     else
     {
-      url = `${this.url(requestParameter)}/${id}`;
+      url = `${this.url(requestParameter)}/${id}${requestParameter.queryString ? `?${requestParameter.queryString}` : ""}`;
     }
     return this.httpClient.delete<T>(url,{headers: requestParameter.headers});
   }
@@ -72,6 +72,7 @@ export class HttpClientService {
 export class RequestParameters{
   controller?:string;
   action?: string;
+  queryString?: string;
 
   headers?: HttpHeaders;
   baseUrl?: string;
--- src/app/services/common/models/product.service.ts
+++ src/app/services/common/models/product.service.ts
@@ -1,6 +1,7 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Create_Product } from 'src/app/contracts/create_product';
+import { List_Product } from 'src/app/contracts/list_product';
 import { HttpClientService } from '../http-client.service';
 
 @Injectable({
@@ -10,7 +11,7 @@ export class ProductService {
 
   constructor(private httpClientServices: HttpClientService) { }
 
-  create(product:Create_Product, succesCallBack?: any, errorCallBack?: any){
+  create(product:Create_Product, succesCallBack?: () => void, errorCallBack?: (errorMessage: string) => void){
     this.httpClientServices.post({
       controller: "product"
     },product).subscribe(result =>{
@@ -25,5 +26,20 @@ export class ProductService {
       });
       errorCallBack(message);
     });
+  }
+
+ async read(
+   page: number = 0,
+   size: number = 5,successCallBack?: ()=> void, 
+   errorCallBack?: (errorMessage: string) => void) : Promise<{totalCount: number; products: List_Product[]}>{
+  const promiseData : Promise<{totalCount: number; products: List_Product[]}> =  this.httpClientServices.get<{totalCount: number; products: List_Product[]}>({
+      controller: "product",
+      queryString : `page=${page}&size=${size}`
+    }).toPromise();
+
+    promiseData.then(d => successCallBack())
+      .catch((errorResponse: HttpErrorResponse) => errorCallBack(errorResponse.message) );
+
+      return await promiseData;
   }
 }
