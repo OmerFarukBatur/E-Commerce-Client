4e0ea1164e8dbc227bc9bf507aeada2918d75ed8
OmerFarukBATUR <44163877+OmerFarukBatur@users.noreply.github.com>
2022-06-10 10:55:42 +0300

    User Registration Form Design (Reactive Form) Has Been Done

--- src/app/ui/components/register/register.component.ts
+++ src/app/ui/components/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, ValidationErrors, AbstractControl } from '@angular/forms';
+import { User } from 'src/app/entities/user';
 
 @Component({
   selector: 'app-register',
@@ -13,42 +14,49 @@ export class RegisterComponent implements OnInit {
 frm: FormGroup;
 
   ngOnInit(): void {
+
     this.frm = this.formBuilder.group({
-      adSoyad: ["",[
-        Validators.required,
+      adSoyad: ['',      
+       ([Validators.required,
         Validators.maxLength(25),
-        Validators.minLength(2)
-      ]],
-      kullaniciAdi: ["",[
-        Validators.required,
+        Validators.minLength(2),])
+       ],
+      kullaniciAdi: ['',        
+        ([Validators.required,
         Validators.maxLength(25),
-        Validators.minLength(2)
-      ]],
-    email: ["",[
-      Validators.required,
+        Validators.minLength(2)])
+      ],
+    email: ['',      
+      ([Validators.required,
       Validators.maxLength(25),
       Validators.minLength(10),
-      Validators.email
-    ]],
-    sifre: ["",
-    ,[
-      Validators.required,
-      Validators.maxLength(25),
-      Validators.minLength(2)
-    ]],
-    sifreTekrar: ["",[
-      Validators.required,
-      Validators.maxLength(25),
-      Validators.minLength(2)
-    ]]
-    })
+      Validators.email])
+    ],
+    sifre: ['',
+    ([Validators.required])
+    ],
+    sifreTekrar: ['',
+      ([Validators.required])
+    ]
+    },{
+      validators: (group: AbstractControl): ValidationErrors | null => {
+        let sifre = group.get("sifre").value;
+        let sifreTekrar = group.get("sifreTekrar").value;
+        return sifre === sifreTekrar ? null : { notSame: true };
+      }
+    });
   }
 
   get component(){
     return this.frm.controls;
   }
 
-  onSubmit(data: any){
-debugger;
+submitted: boolean = false;
+
+  onSubmit(data: User){
+    this.submitted = true;
+    if (this.frm.invalid) {
+      return;
+    }
   }
 }
